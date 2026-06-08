6dff8c79ce90835c135636fd43eba0fd8e925de7
Wilder Taype <i201418741@cibertec.edu.pe>
2026-06-08 10:34:38 -0500

    Actualizacion Principal v10.10.10

--- src/wii.js
+++ src/wii.js
@@ -13,10 +13,10 @@ export let ipdev = import.meta.env.VITE_DEV;
 export let version = 'v10'; // Siempre va "v" para estructura
 
 /** ACTUALIZAR AL TAG POR SEGURIDAD [TAG NUEVO] (1)
-git tag v10 -m "Version v10" ; git push origin v10 
+git tag v10 -m "Version v10" ; git push origin v10  
 
 ACTUALIZACIÓN AL MAIN PRINCIPAL DEL PROYECTO [MAIN] (2)
-git add . ; git commit -m "Actualizacion Principal v10.10.10" ; git push origin main
+git add . ; git commit -m "Actualizacion Principal v10.10.10" ; git push origin main 
 
 // REEMPLAZAR TAG DE SEGURIDAD EXISTENTE [TAG REMPLAZO] (3)
 git tag -d v10 ; git tag v10 -m "Version v10 actualizada" ; git push origin v10 --force
